2ab5f807465f997f867626eae14fbea36fa5a79a
zit-git-jzan <johann@zandanel.net>
2025-04-06 15:26:00 +0200

    2020

--- networks.tf
+++ networks.tf
@@ -4,6 +4,10 @@ resource "upcloud_network" "de-01" {
   name   = "de-01"
   zone   = "de-fra1"
   router = upcloud_router.de-01-router.id
+  labels = {
+    env = "Prod"
+    dep = "Terraform"
+  }
 
   ip_network {
     address            = "10.255.192.0/22"
@@ -11,7 +15,7 @@ resource "upcloud_network" "de-01" {
     dhcp_default_route = true
     family             = "IPv4"
     gateway            = "10.255.192.1"
-    dhcp_dns           = ["192.168.128.10", "8.8.8.8"]
+    dhcp_dns           = ["192.168.128.10", "10.255.192.4", "8.8.8.8"]
   }
 }
 
@@ -19,6 +23,10 @@ resource "upcloud_network" "de-02" {
   name   = "de-02"
   zone   = "de-fra1"
   router = upcloud_router.de-01-router.id
+  labels = {
+    env = "Prod"
+    dep = "Terraform"
+  }
 
   ip_network {
     address            = "10.255.190.0/24"
@@ -26,17 +34,25 @@ resource "upcloud_network" "de-02" {
     dhcp_default_route = true
     family             = "IPv4"
     gateway            = "10.255.190.1"
-    dhcp_dns           = ["8.8.8.8"]
+    dhcp_dns           = ["10.255.192.4", "8.8.8.8"]
   }
 }
 resource "upcloud_router" "de-01-router" {
   name = "de-01"
+  labels = {
+    env = "Prod"
+    dep = "Terraform"
+  }
 }
 
 resource "upcloud_network" "de-03" {
   name   = "de-03"
   zone   = "de-fra1"
   router = upcloud_router.de-01-router.id
+  labels = {
+    env = "Prod"
+    dep = "Terraform"
+  }
 
   ip_network {
     address            = "10.255.191.0/24"
@@ -44,7 +60,7 @@ resource "upcloud_network" "de-03" {
     dhcp_default_route = false
     family             = "IPv4"
     gateway            = "10.255.191.1"
-    dhcp_dns           = ["8.8.8.8"]
+    dhcp_dns           = ["10.255.192.4", "8.8.8.8"]
   }
 }
 
