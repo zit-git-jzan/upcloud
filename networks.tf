86bc654aa5b3c4564bed5db46ca472fb1cf68408
zit-git-jzan <johann@zandanel.net>
2025-05-01 21:00:48 +0200

    2025

--- networks.tf
+++ networks.tf
@@ -1,52 +1,67 @@
-# Zone de-fra1
 
-resource "upcloud_network" "de-01" {
-  name   = "de-01"
-  zone   = "de-fra1"
-  router = upcloud_router.de-01-router.id
 
-  ip_network {
-    address            = "10.255.192.0/22"
-    dhcp               = true
-    dhcp_default_route = true
-    family             = "IPv4"
-    gateway            = "10.255.192.1"
-    dhcp_dns           = ["192.168.128.10", "8.8.8.8"]
-  }
-}
 
 resource "upcloud_network" "de-02" {
   name   = "de-02"
   zone   = "de-fra1"
-  router = upcloud_router.de-01-router.id
+  router = upcloud_router.de-01.id
+  labels = {
+    "dep" = "Terraform"
+    "env" = "Prod"
+  }
 
   ip_network {
     address            = "10.255.190.0/24"
     dhcp               = true
     dhcp_default_route = true
+    dhcp_dns           = ["10.255.192.4", "8.8.8.8"]
     family             = "IPv4"
     gateway            = "10.255.190.1"
-    dhcp_dns           = ["8.8.8.8"]
   }
-}
-resource "upcloud_router" "de-01-router" {
-  name = "de-01"
 }
 
 resource "upcloud_network" "de-03" {
   name   = "de-03"
   zone   = "de-fra1"
-  router = upcloud_router.de-01-router.id
+  router = upcloud_router.de-01.id
+  labels = {
+    "dep" = "Terraform"
+    "env" = "Prod"
+  }
 
   ip_network {
     address            = "10.255.191.0/24"
     dhcp               = true
     dhcp_default_route = false
+    dhcp_dns           = ["10.255.192.4", "8.8.8.8"]
     family             = "IPv4"
     gateway            = "10.255.191.1"
-    dhcp_dns           = ["8.8.8.8"]
   }
 }
 
+resource "upcloud_network" "de-01_2" {
+  name   = "de-01"
+  zone   = "de-fra1"
+  router = upcloud_router.de-01.id
+  labels = {
+    "dep" = "Terraform"
+    "env" = "Prod"
+  }
 
+  ip_network {
+    address            = "10.255.192.0/22"
+    dhcp               = true
+    dhcp_default_route = true
+    dhcp_dns           = ["10.255.192.4", "192.168.128.10", "8.8.8.8"]
+    family             = "IPv4"
+    gateway            = "10.255.192.1"
+  }
+}
 
+resource "upcloud_router" "de-01" {
+  name = "de-01"
+  labels = {
+    "dep" = "Terraform"
+    "env" = "Prod"
+  }
+}
