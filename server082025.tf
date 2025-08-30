d66a6321b1d8dd976543d9fcd0a34daa0e9dd83d
zit-git-jzan <johann@zandanel.net>
2025-08-30 11:45:50 +0200

    Update DEFRAAPP02 server resources and plan
    Changed DEFRAAPP02 server plan from 16GB to 8GB RAM, reduced disk size from 100GB to 80GB, set private IP address, and commented out login block. Updated Terraform state and plan files to reflect these changes.

--- server082025.tf
+++ server082025.tf
@@ -6,17 +6,18 @@ resource "upcloud_server" "DEFRAAPP02" {
   metadata = true
   title    = "defraapp02"
   zone     = "de-fra1"
-  plan     = "DEV-2xCPU-16GB"
+  plan     = "DEV-2xCPU-8GB"
 
   network_interface {
     ip_address_family = "IPv4"
+    ip_address        = "10.255.192.3"
     type              = "private"
     network           = upcloud_network.de-01.id
   }
 
   template {
     storage = "Windows Server 2022 Standard"
-    size    = 100
+    size    = 80
   }
 
   labels = {
@@ -26,11 +27,11 @@ resource "upcloud_server" "DEFRAAPP02" {
     Owner       = "JZAN"
   }
 
-  login {
+  /*   login {
     #user              = "administrator"
     create_password   = true
     password_delivery = "email"
-  }
+  } */
 
   /* user_data = <<-EOT
     #cloud-config
