a92300361b763b04ab98a4c57591149412e1a197
zit-git-jzan <johann@zandanel.net>
2025-09-07 11:07:41 +0200

    Add DEFRADC02 server and update DEFRAAPP02 config
    Added new UpCloud server resource 'DEFRADC02' with Windows Server 2025 Standard in server082025.tf. Updated DEFRAAPP02 server configuration: changed plan to 8GB RAM, updated IP address, storage size, OS version, and removed user_data. Synced state files accordingly.

--- server082025.tf
+++ server082025.tf
@@ -26,6 +26,34 @@ resource "upcloud_server" "DEFRAAPP02" {
     System      = "Windows"
     Owner       = "JZAN"
   }
+}
+
+resource "upcloud_server" "DEFRADC02" {
+  hostname = "defradc02.zandanel.intern"
+  firewall = false
+  metadata = true
+  title    = "defradc02"
+  zone     = "de-fra1"
+  plan     = "DEV-2xCPU-8GB"
+
+  network_interface {
+    ip_address_family = "IPv4"
+    ip_address        = "10.255.192.5"
+    type              = "private"
+    network           = upcloud_network.de-01.id
+  }
+
+  template {
+    storage = "Windows Server 2025 Standard"
+    size    = 80
+  }
+
+  labels = {
+    Environment = "Prod"
+    Project     = "IaaS"
+    System      = "Windows"
+    Owner       = "JZAN"
+  }
 
   /*   login {
     #user              = "administrator"
