72464e8c5874e7b8d94e677855e74734f4ae2452
zit-git-jzan <johann@zandanel.net>
2025-09-18 14:09:05 +0200

    Add static remote route to upcloud gateway connection
    Added a new static remote route 'to-sase' with network 172.31.0.0/22 to the upcloud_gateway_connection 'toVelo' resource in gateway.tf. This change updates the gateway configuration to include routing for the specified network.

--- gateway.tf
+++ gateway.tf
@@ -64,6 +64,11 @@ resource "upcloud_gateway_connection" "toVelo" {
     type           = "static"
     static_network = "10.255.224.0/24"
   }
+  remote_route {
+    name           = "to-sase"
+    type           = "static"
+    static_network = "172.31.0.0/22"
+  }
 }
 
 resource "upcloud_gateway_connection_tunnel" "to-Velo" {
