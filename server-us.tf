f3bdab10e2c829a3a093e63cac715553e846054f
zit-git-jzan <johann@zandanel.net>
2025-10-12 19:14:53 +0200

    Uncomment and update upcloud_server test-us-1 resource
    Uncommented the upcloud_server 'test-us-1' resource in server-us.tf and added a public network interface. Updated terraform state files to reflect the addition of this server and changes in operational state, Terraform version, and serial numbers.

--- server-us.tf
+++ server-us.tf
@@ -1,10 +1,15 @@
-/* resource "upcloud_server" "test-us-1" {
+resource "upcloud_server" "test-us-1" {
   firewall = false
   hostname = "test-us-1"
   metadata = true
   title    = "test-us-1"
   zone     = "us-nyc1"
   plan     = "DEV-1xCPU-2GB"
+
+  network_interface {
+    ip_address_family = "IPv4"
+    type              = "public"
+  }
 
   network_interface {
     ip_address_family = "IPv4"
@@ -33,5 +38,8 @@
   login {
     create_password = false
     keys            = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIn37dO/2MG3ITtzYhMTVDsdNrQAa4rmGgtcR2XTnPWZ zandanelj@zandanelj-Device-iMac-with-OS-AppleOsX-1532.local"]
+    #user = "sysadmin"
+
+    #password_delivery = "email"
   }
-}  */
+} 
